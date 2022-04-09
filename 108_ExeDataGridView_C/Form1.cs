564691794f0c3a8fd5ab3c4826689d1bd3190771
asus <niaaudita01@gmail.com>
2022-04-09 14:16:22 +0800

    Add project files.

---
+++ 108_ExeDataGridView_C/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _108_ExeDataGridView_C
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            // TODO: This line of code loads data into the 'prodiTIDataSet.Mahasiswa' table. You can move, or remove it, as needed.
+            this.mahasiswaTableAdapter.Fill(this.prodiTIDataSet.Mahasiswa);
+
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", 7, FontStyle.Bold);
+
+            dataGridView1.Columns[0].HeaderText = "No. Mahasiswa";
+            dataGridView1.Columns[0].DefaultCellStyle.ForeColor = Color.Purple;
+            dataGridView1.Columns[0].DefaultCellStyle.SelectionBackColor = Color.Pink;
+
+            dataGridView1.Columns[1].HeaderText = "Nama Mahasiswa";
+            dataGridView1.Columns[1].DefaultCellStyle.ForeColor = Color.Blue;
+            dataGridView1.Columns[1].DefaultCellStyle.SelectionBackColor = Color.Orange;
+
+            dataGridView1.Columns[2].HeaderText = "Alamat Mahasiswa";
+            dataGridView1.Columns[2].DefaultCellStyle.ForeColor = Color.LightSeaGreen;
+            dataGridView1.Columns[2].DefaultCellStyle.SelectionBackColor = Color.Green;
+
+            dataGridView1.Columns[3].HeaderText = "Jenis Kelamin";
+            dataGridView1.Columns[3].DefaultCellStyle.ForeColor = Color.Green;
+            dataGridView1.Columns[3].DefaultCellStyle.SelectionBackColor = Color.Blue;
+            dataGridView1.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            dataGridView1.Columns[4].HeaderText = "No. Telpon";
+            dataGridView1.Columns[4].DefaultCellStyle.ForeColor = Color.Maroon;
+            dataGridView1.Columns[4].DefaultCellStyle.SelectionBackColor = Color.Maroon;
+
+            
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
